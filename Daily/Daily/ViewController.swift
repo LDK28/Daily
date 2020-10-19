5b1e282a04d6b952bfd68bcba2339b569a6c774c
Arseny <arseny.tokarev@gmail.com>
2020-10-19 22:07:37 +0300

    Add Xcode Project

---
+++ Daily/Daily/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily
+//
+//  Created by Арсений Токарев on 19.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
