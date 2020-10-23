e991e0098d3d1d21b9ce2d4a21917cbecaa3b915
Arseny <arseny.tokarev@gmail.com>
2020-10-24 00:21:16 +0300

    Improved PlusButton class and added comments

--- Daily/Daily/Views/Plus Button/PlusButton.swift
+++ Daily/Daily/Views/Plus Button/PlusButton.swift
@@ -4,31 +4,39 @@
 //
 //  Created by Арсений Токарев on 23.10.2020.
 //
-
 import UIKit
 
 
+
 class PlusButton: UIButton {
-    
+	
+	final override var frame: CGRect {
+		didSet {
+			decorateButton()
+		}
+	}
+	
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+		decorateButton()
         
     }
 	
-	func designButton() {
+	final private func decorateButton() {
+		//make a plus image take 66% of a button view
 		let lightConfiguration = UIImage.SymbolConfiguration(pointSize: frame.height / 1.5, weight: .light)
-		let image = UIImage(systemName: "plus", withConfiguration: lightConfiguration)?.withTintColor(.white, renderingMode: .alwaysOriginal)
+		
+
+		guard let image = UIImage(systemName: "plus", withConfiguration: lightConfiguration)?.withTintColor(.white, renderingMode: .alwaysOriginal)
+		else {
+			fatalError("Couldn't find the image from systemName.")
+		}
 	
 		self.setImage(image, for: .normal)
-		
-		self.currentBackgroundImage?.withTintColor(.white, renderingMode: .alwaysOriginal)
-		
-		self.titleLabel?.textColor = .white
 		self.backgroundColor = .dailyTabBarColor
 		
 		self.layer.cornerRadius = frame.height / 2
--- Daily/Daily/Views/Tab bar/TabBarVC.swift
+++ Daily/Daily/Views/Tab bar/TabBarVC.swift
@@ -18,39 +18,47 @@ class TabBarVC: UITabBarController {
 	
 	
 	func setupMiddleButton() {
-
-		plusButton.frame = CGRect(x: 0, y: 0, width: tabBar.frame.size.height * 1.2, height: tabBar.frame.size.height * 1.2)
+		//set sizes of the button accordingly to bound of a user's device
+		//"x1.2" is just a multiplayer that makes this button a little bit bigger
+		let multiplier: CGFloat = 1.2
+		let size = tabBar.frame.size.height * multiplier
+		plusButton.frame = CGRect(x: 0, y: 0, width: size, height: size)
+		
+		//framing the button (for further resizing)
 		var plusButtonFrame = plusButton.frame
+		
 		plusButtonFrame.origin.y = self.view.bounds.height - plusButtonFrame.height - self.view.safeAreaInsets.bottom
-		plusButtonFrame.origin.x = self.view.bounds.width/2 - plusButtonFrame.size.width/2
+		plusButtonFrame.origin.x = self.view.bounds.width / 2 - plusButtonFrame.size.width / 2
+		
 		plusButton.frame = plusButtonFrame
-		plusButton.designButton()
+		
 		self.view.addSubview(plusButton)
 		self.view.layoutIfNeeded()
 	}
 
 	override func viewDidLayoutSubviews() {
 	   super.viewDidLayoutSubviews()
-		plusButton.frame.origin.y = self.view.bounds.height - plusButton.frame.height * 1.45 - self.view.safeAreaInsets.bottom
+		let multiplier: CGFloat = 1.45
+		plusButton.frame.origin.y = self.view.bounds.height - plusButton.frame.height * multiplier - self.view.safeAreaInsets.bottom
    }
 	
 	func createTabBar() {
-		// here we assign a tint to all our tabbars, this will be visible on the items (icons)
+
 		let styling = self.tabBar
 		styling.isTranslucent = false
 		styling.tintColor = .dailyTabBarSelectedItemColor
 		styling.unselectedItemTintColor = .dailyTabBarItemColor
 		styling.barTintColor = .dailyTabBarColor
 
-		// replace our array variables with the functions we created
+	
 		self.viewControllers = [createDiaryNavigationController(), createIdeasNavigationController(), createCalendarNavigationController(), createProfileNavigationController()]
 	}
 	
 	func createDiaryNavigationController() -> UINavigationController {
-		// we create the view controller and insert into the nav controller and return
+	
 		let diaryVC = DiaryVC()
 		diaryVC.title = nil
-		// it is a system tabbaritem , tag zero because it is the first one
+		
 		diaryVC.tabBarItem = UITabBarItem(title: "Diary", image: UIImage(systemName: "note.text.badge.plus"), tag: 0)
 		let diaryNavigationController =  UINavigationController(rootViewController: diaryVC)
 		diaryNavigationController.setNavigationBarHidden(true, animated: false)
@@ -62,7 +70,7 @@ class TabBarVC: UITabBarController {
 	func createIdeasNavigationController() -> UINavigationController {
 		let ideasVC = IdeasVC()
 		ideasVC.title = nil
-		// it is a system tabbaritem , tag zero because it is the first one
+		
 		ideasVC.tabBarItem = UITabBarItem(title: "Ideas", image: UIImage(systemName: "note"), tag: 1)
 		let ideaNavigationController = UINavigationController(rootViewController: ideasVC)
 		
@@ -74,7 +82,7 @@ class TabBarVC: UITabBarController {
 	func createCalendarNavigationController() -> UINavigationController {
 		let calendarVC = CalendarVC()
 		calendarVC.title = nil
-		// it is a system tabbaritem , tag zero because it is the first one
+		
 		calendarVC.tabBarItem = UITabBarItem(title: "Calendar", image: UIImage(systemName: "calendar.circle.fill"), tag: 2)
 		let calendarNavigationController = UINavigationController(rootViewController: calendarVC)
 		calendarNavigationController.setNavigationBarHidden(true, animated: false)
