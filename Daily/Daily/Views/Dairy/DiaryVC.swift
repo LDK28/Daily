1f9eefc9eff544e4e920f298777721e6f15ba8e8
JuliaAlfimova <juliemoorled@gmail.com>
2020-10-23 20:48:23 +0300

    Added PlusButton

--- Daily/Daily/Views/Dairy/DiaryVC.swift
+++ Daily/Daily/Views/Dairy/DiaryVC.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class DiaryVC: UIViewController {
 
+    let plusButton = PlusButton(frame: CGRect(x: 32, y: 32, width: 50, height: 50))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 		
 		view.backgroundColor = UIColor("E5E5E5")
 		view.lightLG()
+        
+        view.addSubview(plusButton)
     }
 
 }
--- Daily/Daily/Views/Plus Button/PlusButton.swift
+++ Daily/Daily/Views/Plus Button/PlusButton.swift
@@ -8,6 +8,23 @@
 import UIKit
 
 
-class PlussButton: UIButton {
-	
+class PlusButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setBackgroundImage(UIImage(systemName: "plus"), for: .normal)
+        self.titleLabel?.textColor = .white
+        self.backgroundColor = .dailyTabBarColor
+        self.layer.cornerRadius = frame.height/2
+        self.layer.borderWidth = 4
+        self.layer.borderColor = UIColor.white.cgColor
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+    }
+    
 }
