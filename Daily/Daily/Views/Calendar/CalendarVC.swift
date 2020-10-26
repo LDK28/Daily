fa19baf4e43cad77c8e17945e87ad3fcf5c777d8
JuliaAlfimova <juliemoorled@gmail.com>
2020-10-26 15:27:03 +0300

    Added calendar view

--- Daily/Daily/Views/Calendar/CalendarVC.swift
+++ Daily/Daily/Views/Calendar/CalendarVC.swift
@@ -8,11 +8,46 @@
 import UIKit
 import FSCalendar
 
-class CalendarVC: MainVC {
+class CalendarVC: MainVC, FSCalendarDelegate, FSCalendarDataSource {
 
+    var calendarView = UIView()
+    var calendar = FSCalendar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 		
+        calendar.delegate = self
+        
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        calendarView.frame = CGRect(x: 20, y: 44, width: view.frame.size.width - 40, height: view.frame.size.width)
+        calendarView.backgroundColor = .white
+        calendarView.layer.cornerRadius = 10
+        view.addSubview(calendarView)
+        
+        calendar.frame = CGRect(x: 30, y: 44, width: view.frame.size.width - 60, height: view.frame.size.width)
+        calendar.appearance.titleFont = UIFont(name: "Stolzl-Book", size: 15)
+        calendar.appearance.weekdayFont = UIFont(name: "Stolzl-Book", size: 15)
+        calendar.appearance.weekdayTextColor = .systemGray
+        calendar.appearance.subtitleFont = UIFont(name: "Stolzl-Book", size: 15)
+        calendar.appearance.headerTitleFont = UIFont(name: "Stolzl-Regular", size: 18)
+        calendar.appearance.headerTitleColor = .dailyTextColor
+        calendar.appearance.todayColor = .systemRed
+        calendar.appearance.selectionColor = .systemGray
+        view.addSubview(calendar)
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "EEEE dd.MM.YYYY"
+//        let selectedDate = (formatter.string(from: date)).split(separator: " ")
+//        let dayOfTheWeek = String(selectedDate[0])
+//        dateLabel.text = dayOfTheWeek
+        
     }
     
 
