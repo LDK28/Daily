c38e4765b8363b6451ca30f39caf4aaa7093fa5f
Arseny <arseny.tokarev@gmail.com>
2020-10-27 22:27:27 +0300

    Structured code into blocks

--- Daily/Daily/Views/Calendar/CalendarVC.swift
+++ Daily/Daily/Views/Calendar/CalendarVC.swift
@@ -16,67 +16,85 @@ class CalendarVC: MainVC, FSCalendarDelegate, FSCalendarDataSource {
     
     override func loadView() {
         super.loadView()
-        
-        headerLabel.translatesAutoresizingMaskIntoConstraints = false
-        headerLabel.font = UIFont(name: "Stolzl-Bold", size: 36)
-        headerLabel.textColor = .dailyTitleTextColor
-        addShadow(to: headerLabel)
-        headerLabel.textAlignment = .center
-        headerLabel.text = "Calendar"
-        
-        calendarView.translatesAutoresizingMaskIntoConstraints = false
-        calendarView.backgroundColor = .dailyCalendarBackgroundColor
-        calendarView.layer.cornerRadius = 10
-        addShadow(to: calendarView)
-        calendarView.appearance.titleFont = UIFont(name: "Stolzl-Book", size: 15)
-        calendarView.appearance.titleDefaultColor = .dailyTextColor
-        calendarView.appearance.weekdayFont = UIFont(name: "Stolzl-Book", size: 15)
-        calendarView.appearance.weekdayTextColor = .systemGray
-        calendarView.appearance.headerTitleFont = UIFont(name: "Stolzl-Regular", size: 18)
-        calendarView.appearance.headerTitleColor = .dailyTextColor
-        calendarView.appearance.todayColor = .systemRed
-        calendarView.appearance.selectionColor = .systemGray
-        
-        dayView.translatesAutoresizingMaskIntoConstraints = false
-        dayView.backgroundColor = .dailyCalendarBackgroundColor
-        addShadow(to: dayView)
-        dayView.layer.cornerRadius = 10
+		
+		view.addSubview(headerLabel)
+		view.addSubview(calendarView)
+		view.addSubview(dayView)
+		
+		configureHeaderLabel()
+		configureCalendarView()
+		configureDayView()
+		
+		styleHeaderLabel()
+		styleCalendarView()
+		styleDayView()
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-		
         calendarView.delegate = self
-        
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        view.addSubview(headerLabel)
-        view.addSubview(calendarView)
-        view.addSubview(dayView)
-        
-        NSLayoutConstraint.activate([
-            
-            headerLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 16),
-            headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            headerLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
-            
-            calendarView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 16),
-            calendarView.centerXAnchor.constraint(equalTo: headerLabel.centerXAnchor),
-            calendarView.widthAnchor.constraint(equalTo: headerLabel.widthAnchor, multiplier: 1),
-            calendarView.heightAnchor.constraint(equalTo: calendarView.widthAnchor, multiplier: 1),
-            
-            dayView.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 16),
-            dayView.centerXAnchor.constraint(equalTo: calendarView.centerXAnchor),
-            dayView.widthAnchor.constraint(equalTo: calendarView.widthAnchor, multiplier: 1),
-            dayView.heightAnchor.constraint(equalTo: calendarView.widthAnchor, multiplier: 0.5)
-            //не всегда влезает на экран :(
-        ])
-        
-    }
+	
+	func configureHeaderLabel() {
+		NSLayoutConstraint.activate([
+			headerLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 16),
+			headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			headerLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+		])
+	}
+	
+	func configureCalendarView() {
+		NSLayoutConstraint.activate([
+			calendarView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 16),
+			calendarView.centerXAnchor.constraint(equalTo: headerLabel.centerXAnchor),
+			calendarView.widthAnchor.constraint(equalTo: headerLabel.widthAnchor, multiplier: 1),
+			calendarView.heightAnchor.constraint(equalTo: calendarView.widthAnchor, multiplier: 1),
+		])
+	}
+	
+	func configureDayView() {
+		NSLayoutConstraint.activate([
+			dayView.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 16),
+			dayView.centerXAnchor.constraint(equalTo: calendarView.centerXAnchor),
+			dayView.widthAnchor.constraint(equalTo: calendarView.widthAnchor, multiplier: 1),
+			dayView.heightAnchor.constraint(equalTo: calendarView.widthAnchor, multiplier: 0.5)
+			//не всегда влезает на экран :(
+		])
+	}
+	
+	
+	func styleHeaderLabel() {
+		headerLabel.translatesAutoresizingMaskIntoConstraints = false
+		headerLabel.font = UIFont(name: "Stolzl-Bold", size: 36)
+		headerLabel.textColor = .dailyTitleTextColor
+		addShadow(to: headerLabel)
+		headerLabel.textAlignment = .center
+		headerLabel.text = "Calendar"
+	}
+	
+	func styleCalendarView() {
+		calendarView.translatesAutoresizingMaskIntoConstraints = false
+		calendarView.backgroundColor = .dailyCalendarBackgroundColor
+		calendarView.layer.cornerRadius = 10
+		addShadow(to: calendarView)
+		calendarView.appearance.titleFont = UIFont(name: "Stolzl-Book", size: 15)
+		calendarView.appearance.titleDefaultColor = .dailyTextColor
+		calendarView.appearance.weekdayFont = UIFont(name: "Stolzl-Book", size: 15)
+		calendarView.appearance.weekdayTextColor = .systemGray
+		calendarView.appearance.headerTitleFont = UIFont(name: "Stolzl-Regular", size: 18)
+		calendarView.appearance.headerTitleColor = .dailyTextColor
+		calendarView.appearance.todayColor = .systemRed
+		calendarView.appearance.selectionColor = .systemGray
+	}
+	
+	func styleDayView() {
+		dayView.translatesAutoresizingMaskIntoConstraints = false
+		dayView.backgroundColor = .dailyCalendarBackgroundColor
+		addShadow(to: dayView)
+		dayView.layer.cornerRadius = 10
+	}
+	
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         //обработка события нажатия на день
@@ -87,16 +105,5 @@ class CalendarVC: MainVC, FSCalendarDelegate, FSCalendarDataSource {
         view.layer.shadowOffset = CGSize(width: 0, height: 10)
         view.layer.shadowRadius = 4
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
