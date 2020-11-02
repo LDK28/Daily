3eebf920b1f894d551493c054cbfae37d2d7268c
Arseny <arseny.tokarev@gmail.com>
2020-11-02 22:54:00 +0300

    Prepared overlays view controllers

--- Daily/Daily/Views/Tab bar/TabBarVC.swift
+++ Daily/Daily/Views/Tab bar/TabBarVC.swift
@@ -7,10 +7,10 @@
 import UIKit
 import FirebaseAuth
 
-class TabBarVC: UITabBarController {
+class TabBarVC: TabBarControllerWithMiddleButton {
 	let plusButton = PlusButton()
 	
-	let plusButtonBlackout: UIView = {
+	let blackoutView: UIView = {
 		let view = UIView()
 		view.translatesAutoresizingMaskIntoConstraints = false
 		view.backgroundColor = .dailyPlusButtonBlackoutColor
@@ -36,13 +36,12 @@ class TabBarVC: UITabBarController {
 		addButtonsStackView.addArrangedSubview(newNoteButton)
 		addButtonsStackView.addArrangedSubview(newTaskButton)
 		
-		view.addSubview(plusButtonBlackout)
+		view.addSubview(blackoutView)
 		view.addSubview(plusButton)
 		view.addSubview(addButtonsStackView)
 		
-		createTabBar()
-		setupBlackoutForPlusButton()
-		setupPlusButton()
+		layoutMiddleButton(button: plusButton)
+		setupBlackoutView()
 		setupAddButtonsVerticalStackView()
 		
 		styleElements()
@@ -67,16 +66,16 @@ class TabBarVC: UITabBarController {
 		
 		if plusButton.isPressedToShowOverlay {
 			addButtonsStackView.isHidden = false //show stack
-			plusButtonBlackout.isHidden = false //blackout the background
+			blackoutView.isHidden = false //blackout the background
 			UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut) {
-				self.plusButtonBlackout.alpha = 1 //and show it with animation
+				self.blackoutView.alpha = 1 //and show it with animation
 			}
 		} else {
 			UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
 				self.addButtonsStackView.isHidden = true
-				self.plusButtonBlackout.alpha = 0
+				self.blackoutView.alpha = 0
 			}) { _ in
-				self.plusButtonBlackout.isHidden = true
+				self.blackoutView.isHidden = true
 			}
 		}
 	}
@@ -104,86 +103,19 @@ class TabBarVC: UITabBarController {
 		
 	}
 	
-	func setupBlackoutForPlusButton() {
-		plusButtonBlackout.isHidden = true
-		plusButtonBlackout.alpha = 0
+	func setupBlackoutView() {
+		blackoutView.isHidden = true
+		blackoutView.alpha = 0
 		NSLayoutConstraint.activate([
-			plusButtonBlackout.topAnchor.constraint(equalTo: view.topAnchor),
-			plusButtonBlackout.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -tabBar.frame.height),
-			plusButtonBlackout.widthAnchor.constraint(equalTo: view.widthAnchor),
+			blackoutView.topAnchor.constraint(equalTo: view.topAnchor),
+			blackoutView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -tabBar.frame.height),
+			blackoutView.widthAnchor.constraint(equalTo: view.widthAnchor),
 		])
-	}
-	
-	func setupPlusButton() {
-		
-		let multiplier: CGFloat = 1.2
-		let size = tabBar.frame.size.height * multiplier
-		
-		NSLayoutConstraint.activate([
-			plusButton.centerXAnchor.constraint(equalTo: tabBar.centerXAnchor),
-			plusButton.centerYAnchor.constraint(equalTo: tabBar.safeAreaLayoutGuide.topAnchor, constant: -5),
-			plusButton.heightAnchor.constraint(equalToConstant: size),
-			plusButton.widthAnchor.constraint(equalTo: plusButton.heightAnchor)
-		])
-		
 	}
 	
 	func styleElements() {
 		
 		addButtonsStackView.styleStackView(spacing: 10, axis: .vertical)
 	}
-	
-	func createTabBar() {
-
-		let styling = tabBar
-		styling.isTranslucent = false
-		styling.tintColor = .dailyTabBarSelectedItemColor
-		styling.unselectedItemTintColor = .dailyTabBarItemColor
-		styling.barTintColor = .dailyTabBarColor
-		let systemFontAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14)]
-		UITabBarItem.appearance().setTitleTextAttributes(systemFontAttributes, for: .normal)
-
-	
-		self.viewControllers = [createDiaryNC(), createIdeasNC(), createCalendarNC(), createProfileNC()]
-	}
-	
-	func createDiaryNC() -> UINavigationController {
-	
-		let diaryVC = DiaryVC()
-		diaryVC.title = nil
-		diaryVC.tabBarItem = UITabBarItem(title: "Diary", image: UIImage(systemName: "note.text.badge.plus"), tag: 0)
-		let diaryNavigationController =  UINavigationController(rootViewController: diaryVC)
-		diaryNavigationController.setNavigationBarHidden(true, animated: false)
-		return diaryNavigationController
-		
-	}
-
-	func createIdeasNC() -> UINavigationController {
-		let ideasVC = IdeasVC()
-		ideasVC.title = nil
-		ideasVC.tabBarItem = UITabBarItem(title: "Ideas", image: UIImage(systemName: "note"), tag: 1)
-		let ideaNavigationController = UINavigationController(rootViewController: ideasVC)
-		ideaNavigationController.setNavigationBarHidden(true, animated: false)
-		return ideaNavigationController
-	}
-
-	func createCalendarNC() -> UINavigationController {
-		let calendarVC = CalendarVC()
-		calendarVC.title = nil
-		calendarVC.tabBarItem = UITabBarItem(title: "Calendar", image: UIImage(systemName: "calendar.circle.fill"), tag: 2)
-		let calendarNavigationController = UINavigationController(rootViewController: calendarVC)
-		calendarNavigationController.setNavigationBarHidden(true, animated: false)
-		return calendarNavigationController
-	}
-	
-	func createProfileNC() -> UINavigationController {
-		let userInfoVC = UserInfoVC()
-		userInfoVC.title = nil
-		userInfoVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.crop.circle"), tag: 3)
-		let profileNavigationController = UINavigationController(rootViewController: userInfoVC)
-		profileNavigationController.setNavigationBarHidden(true, animated: false)
-		return profileNavigationController
-	}
-	
 
 }
