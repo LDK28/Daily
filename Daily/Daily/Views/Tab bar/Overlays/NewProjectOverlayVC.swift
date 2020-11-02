4010767061b62044f1bdee2b23f39d1ca6596513
Arseny <arseny.tokarev@gmail.com>
2020-11-02 22:54:00 +0300

    Prepared overlays view controllers

---
+++ Daily/Daily/Views/Tab bar/Overlays/NewProjectOverlayVC.swift
@@ -0,0 +1,29 @@
+//
+//  NewProjectOverlayVC.swift
+//  Daily
+//
+//  Created by Арсений Токарев on 02.11.2020.
+//
+
+import UIKit
+
+class NewProjectOverlayVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
