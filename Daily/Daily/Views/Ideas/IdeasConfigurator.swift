738600d1efc6f72094f568e54010a041523d490f
JuliaAlfimova <juliemoorled@gmail.com>
2020-11-22 23:21:10 +0300

    changed configurator

--- Daily/Daily/Views/Ideas/IdeasConfigurator.swift
+++ Daily/Daily/Views/Ideas/IdeasConfigurator.swift
@@ -10,24 +10,14 @@ import UIKit
 class IdeasModule {
     static func build() -> IdeasVC {
         
-        let dataSource = [IdeasCellViewModel(title: "Notes", hasStatistics: false)]
+        let dataSource = [IdeasCellViewModel(title: "Notes", hasStatistics: false), IdeasCellViewModel(title: "Projects", hasStatistics: false)]
         
         let viewController = IdeasVC()
         let presenter = IdeasPresenter(viewController: viewController)
         let interactor = IdeasInteractor(presenter: presenter, dataSource: dataSource)
         let router = IdeasRouter(viewController: viewController, dataStore: interactor)
         
-        
-        //presenter.dataSource = dataSource
-        //presenter.viewController = viewController
-        
-        //let router = IdeasRouter()
-        //router.viewController = viewController
         viewController.router = router
-        
-        //interactor.dataSource = dataSource
-        //interactor.presenter = presenter
-        //router.dataStore = interactor
         viewController.interactor = interactor
         
         return viewController
