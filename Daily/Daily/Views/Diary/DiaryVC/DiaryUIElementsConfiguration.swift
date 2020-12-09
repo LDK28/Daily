fbc700a26250e975e4631d29874f54568a4278cf
LDK28 <dkostev68@gmail.com>
2020-12-09 18:19:55 +1100

    Update architecture

--- Daily/Daily/Views/Diary/DiaryVC/DiaryUIElementsConfiguration.swift
+++ Daily/Daily/Views/Diary/DiaryVC/DiaryUIElementsConfiguration.swift
@@ -1,8 +1,91 @@
-//
-//  DiaryUIElementsConfiguration.swift
-//  Daily
-//
-//  Created by Владимир Проценко on 09.12.2020.
-//
-
 import Foundation
+import UIKit
+
+extension DiaryVC {
+    internal func configureHeaderLabel() {
+        // MARK: - Header style
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerLabel.font = UIFont(name: "Stolzl-Bold", size: 36)
+        headerLabel.textColor = .dailyTitleTextColor
+        addShadow(to: headerLabel)
+        headerLabel.textAlignment = .right
+        switch Calendar.current.component(.weekday, from: Date()){
+        case 1:
+            headerLabel.text = "Sunday"
+        case 2:
+            headerLabel.text = "Monday"
+        case 3:
+            headerLabel.text = "Tuesday"
+        case 4:
+            headerLabel.text = "Wednesday"
+        case 5:
+            headerLabel.text = "Thursday"
+        case 6:
+            headerLabel.text = "Friday"
+        case 7:
+            headerLabel.text = "Saturday"
+        default:
+            headerLabel.text = "Date"
+        }
+        
+        // MARK: - Header constraints
+        NSLayoutConstraint.activate([
+            headerLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 16),
+            headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            headerLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+        ])
+    }
+    
+    internal func configureSubheaderLabel() {
+        // MARK: - Subheader style
+        subheaderLabel.translatesAutoresizingMaskIntoConstraints = false
+        subheaderLabel.font = UIFont(name: "Stolzl-Bold", size: 18)
+        subheaderLabel.textColor = .dailyTitleTextColor
+        addShadow(to: subheaderLabel)
+        subheaderLabel.textAlignment = .right
+        subheaderLabel.text = Date().asString(style: .long)
+        
+        // MARK: - Subheader constraints
+        NSLayoutConstraint.activate([
+            subheaderLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 56),
+            subheaderLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            subheaderLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+        ])
+    }
+    
+    internal func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.tableFooterView = UIView(frame: .zero)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 200
+        
+        tableView.register(DiaryCell.self, forCellReuseIdentifier: DiaryCell.identifier)
+        tableView.register(DiaryHeader.self, forHeaderFooterViewReuseIdentifier: DiaryHeader.identifier)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 90),
+            tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            tableView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+    }
+    
+    internal func addShadow(to view: UIView){
+        view.layer.shadowOpacity = 0.05
+        view.layer.shadowOffset = CGSize(width: 0, height: 10)
+        view.layer.shadowRadius = 4
+    }
+}
+
+// MARK: - Date -> String
+extension Date {
+  func asString(style: DateFormatter.Style) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = style
+    return dateFormatter.string(from: self)
+  }
+}
--- Daily/Daily/Views/Diary/DiaryVC/DiaryVC.swift
+++ Daily/Daily/Views/Diary/DiaryVC/DiaryVC.swift
@@ -3,18 +3,13 @@ import UIKit
 
 class DiaryVC: MainVC {
     
-    let myDate = Date()
-    let weekday = Calendar.current.component(.weekday, from: Date())
-    private let tableView = UITableView(frame: CGRect.zero, style: .grouped)
-
-    // MARK: - External vars
+    internal var headerLabel = UILabel()
+    internal var subheaderLabel = UILabel()
+    internal let tableView = UITableView(frame: CGRect.zero, style: .grouped)
     
-    // MARK: - Internal vars
+    var dataToDisplay = [DiaryCellModel]()
     private var interactor: DiaryBusinessLogic?
-    private var dataToDisplay = [DiaryCellModel]()
-    var headerLabel = UILabel()
-    var subheaderLabel = UILabel()
-    
+
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         setup()
@@ -51,85 +46,6 @@ class DiaryVC: MainVC {
         super.viewDidLoad()
         interactor?.fetchDiary()
     }
-    
-    func configureHeaderLabel() {
-        // MARK: - Header style
-        headerLabel.translatesAutoresizingMaskIntoConstraints = false
-        headerLabel.font = UIFont(name: "Stolzl-Bold", size: 36)
-        headerLabel.textColor = .dailyTitleTextColor
-        addShadow(to: headerLabel)
-        headerLabel.textAlignment = .right
-        switch weekday{
-        case 1:
-            headerLabel.text = "Sunday"
-        case 2:
-            headerLabel.text = "Monday"
-        case 3:
-            headerLabel.text = "Tuesday"
-        case 4:
-            headerLabel.text = "Wednesday"
-        case 5:
-            headerLabel.text = "Thursday"
-        case 6:
-            headerLabel.text = "Friday"
-        case 7:
-            headerLabel.text = "Saturday"
-        default:
-            headerLabel.text = "Date"
-        }
-        
-        // MARK: - Header constraints
-        NSLayoutConstraint.activate([
-            headerLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 16),
-            headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            headerLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
-        ])
-    }
-    
-    func configureSubheaderLabel() {
-        // MARK: - Subheader style
-        subheaderLabel.translatesAutoresizingMaskIntoConstraints = false
-        subheaderLabel.font = UIFont(name: "Stolzl-Bold", size: 18)
-        subheaderLabel.textColor = .dailyTitleTextColor
-        addShadow(to: subheaderLabel)
-        subheaderLabel.textAlignment = .right
-        subheaderLabel.text = myDate.asString(style: .long)
-        
-        // MARK: - Subheader constraints
-        NSLayoutConstraint.activate([
-            subheaderLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 56),
-            subheaderLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            subheaderLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
-        ])
-    }
-    
-    func configureTableView() {
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.backgroundColor = .clear
-        tableView.tableFooterView = UIView(frame: .zero)
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 200
-        
-        tableView.register(DiaryCell.self, forCellReuseIdentifier: DiaryCell.identifier)
-        tableView.register(DiaryHeader.self, forHeaderFooterViewReuseIdentifier: DiaryHeader.identifier)
-        
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 90),
-            tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            tableView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -20),
-            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
-        ])
-    }
-    
-    func addShadow(to view: UIView){
-        view.layer.shadowOpacity = 0.05
-        view.layer.shadowOffset = CGSize(width: 0, height: 10)
-        view.layer.shadowRadius = 4
-    }
-
     
 }
 
@@ -177,13 +93,4 @@ extension DiaryVC: DiaryDisplayLogic {
         dataToDisplay.append(contentsOf: data)
         tableView.reloadData()
     }
-}
-
-// MARK: - Date -> String
-extension Date {
-  func asString(style: DateFormatter.Style) -> String {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateStyle = style
-    return dateFormatter.string(from: self)
-  }
 }
